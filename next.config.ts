0de8945691d6f62a79596f97708000be030f24f8
Nothing22-06 <deivi.ha.22@gmail.com>
2025-07-22 14:58:40 -0500

    commit portafolio

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    output: 'export',
+    trailingSlash: true,
+    images: {
+        unoptimized: true
+    },
+    basePath: '/Portafolio', // Nombre de tu repositorio
+    assetPrefix: '/Portafolio/', // Nombre de tu repositorio
 };
 
 export default nextConfig;
+
